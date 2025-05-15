a38696d9eacfa529dd4e069c332580132e58e31f
subramanayam95 <cheenepallisubramanyam95@gmail.com>
2025-05-15 15:48:20 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Subramanyam",
+    author_email="cheenepallisubramanyam95@gmail.com",
+    packages=find_packages()
+)
